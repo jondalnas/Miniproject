a4738a1368abc7d7af1a17f2176506459b2fadd3
Jonas Jensen <jdudal11@gmail.com>
2019-11-26 22:30:59 +0100

    Added sword rotation

--- src/com/JoL/Miniproject/entities/Player.java
+++ src/com/JoL/Miniproject/entities/Player.java
@@ -2,7 +2,9 @@ package com.JoL.Miniproject.entities;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 
@@ -14,8 +16,9 @@ public class Player extends GravityEntity {
 	private double speed = 256;
 	private double jumpSpeed = 512;
 	
-	private double swordX, swordY;
-	private BufferedImage sword;
+	private double swordRotation;
+	private final BufferedImage sword;
+	private final AffineTransform at;
 	
 	public Player() {
 		super(new Color(255, 0, 255), 64, 64);
@@ -26,16 +29,16 @@ public class Player extends GravityEntity {
 		
 		swordG.fillPolygon(new int[] {0, 12, 12, 16, 16, 64, 60, 16, 16, 12, 12, 0},
 					  new int[] {12, 12, 0, 4, 12, 12, 16, 16, 24, 28, 16, 16}, 12);
+		
+		at = new AffineTransform();
 	}
 	
 	public void tick() {
 		//Calculate attack
-		double mousePlayerX = Input.mousePos[0] - Main.WIDTH / 2;
-		double mousePlayerY = Input.mousePos[1] - Main.HEIGHT / 2;
-		
-		double d = Math.sqrt(mousePlayerX*mousePlayerX + mousePlayerY*mousePlayerY);
-		swordX = mousePlayerX / d * width;
-		swordY = mousePlayerY / d * height;
+		double swordX = (Input.mousePos[0] - Main.WIDTH / 2) * width;
+		double swordY = (Input.mousePos[1] - Main.HEIGHT / 2) * height;
+
+		swordRotation = Math.atan(swordY/swordX) + (swordX < 0 ? Math.PI : 0);
 		
 		if (Input.mouseButtons[1]) {
 			
@@ -56,7 +59,11 @@ public class Player extends GravityEntity {
 	public void render(Graphics g) {
 		g.setColor(entityColor);
 		g.fillRect(Main.WIDTH/2 - (int) width/2, Main.HEIGHT/2 - (int) height/2, (int) width, (int) height);
-		
-		g.drawImage(sword, (int) swordX + Main.WIDTH/2, (int) swordY + Main.HEIGHT/2, null);
+
+		at.setToRotation(swordRotation , Main.WIDTH/2, Main.HEIGHT/2);
+		((Graphics2D) g).setTransform(at);
+		g.drawImage(sword, Main.WIDTH/2, Main.HEIGHT/2, null);
+		at.setToRotation(0);
+		((Graphics2D) g).setTransform(at);
 	}
 }
