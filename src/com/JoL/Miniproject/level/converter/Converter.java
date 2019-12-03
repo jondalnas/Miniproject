59cf75288780b2de1eb45b9c33a7ff26d0837dec
Jonas Jensen <jdudal11@gmail.com>
2019-12-03 22:57:44 +0100

    Updated level converter, but it might not amount to anything

--- src/com/JoL/Miniproject/level/converter/Converter.java
+++ src/com/JoL/Miniproject/level/converter/Converter.java
@@ -2,11 +2,15 @@ package com.JoL.Miniproject.level.converter;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import com.JoL.Miniproject.level.LevelLoader;
 
 public class Converter {
+	private static final double MM_TO_PIXEL = 64/16.933333;
+	
 
 	public static void main(String[] args) {
 		convert("/level.svg", "HI");
@@ -20,21 +24,102 @@ public class Converter {
 			throw new RuntimeException(e);
 		}
 		
+		List<double[][]> polygons = new ArrayList<double[][]>();
+		
 		boolean inPath = false;
 		while (scan.hasNext()) {
 			String line = scan.next();
 			if (inPath) {
-				System.out.println(line);
+				line = line.toLowerCase();
 				if (!line.contains("id=\"") && line.contains("d=\"")) {
-					//System.out.println(line);
-					//line = line.substring(line.indexOf('"'), line.toLowerCase().lastIndexOf('z')-1);
-					//System.out.println(line);
+					line = line.substring(line.indexOf("\"")+1);
+					String result = line;
+					
+					while (!(line = scan.nextLine()).contains("Z")) result += line;
+					
+					result += line;
+					result = result.substring(0, result.indexOf('Z'));
+					
+					String[] instructions = result.split(" ");
+					
+					List<double[]> coords = new ArrayList<double[]>();
+					double[] currCord = new double[2];
+					char instruction = ' ';
+					for (int i = 0; i < instructions.length; i++) {
+						if ("mlvhMLVH".contains(instructions[i].charAt(0)+"")) {
+							instruction = instructions[i].charAt(0);
+							i++;
+						}
+						
+						String numbers = instructions[i];
+						
+						switch (instruction) {
+						case 'm': {
+							String[] strNums = numbers.split(",");
+							double[] nums = new double[] {Double.parseDouble(strNums[0]), Double.parseDouble(strNums[1])};
+
+							currCord[0] = nums[0] * MM_TO_PIXEL;
+							currCord[1] = nums[1] * MM_TO_PIXEL;
+							break;
+						}
+						case 'l': {
+							String[] strNums = numbers.split(",");
+							double[] nums = new double[] {Double.parseDouble(strNums[0]), Double.parseDouble(strNums[1])};
+
+							currCord[0] += nums[0] * MM_TO_PIXEL;
+							currCord[1] += nums[1] * MM_TO_PIXEL;
+							break;
+						}
+						case 'v': {
+							currCord[0] += Double.parseDouble(numbers) * MM_TO_PIXEL;
+							break;
+						}
+						case 'h': {
+							currCord[1] += Double.parseDouble(numbers) * MM_TO_PIXEL;
+							break;
+						}
+						case 'M': {
+							String[] strNums = numbers.split(",");
+							double[] nums = new double[] {Double.parseDouble(strNums[0]), Double.parseDouble(strNums[1])};
+
+							currCord[0] = nums[0] * MM_TO_PIXEL;
+							currCord[1] = nums[1] * MM_TO_PIXEL;
+							break;
+						}
+						case 'L': {
+							String[] strNums = numbers.split(",");
+							double[] nums = new double[] {Double.parseDouble(strNums[0]), Double.parseDouble(strNums[1])};
+
+							currCord[0] += nums[0] * MM_TO_PIXEL;
+							currCord[1] += nums[1] * MM_TO_PIXEL;
+							break;
+						}
+						case 'V': {
+							currCord[0] += Double.parseDouble(numbers) * MM_TO_PIXEL;
+							break;
+						}
+						case 'H': {
+							currCord[1] += Double.parseDouble(numbers) * MM_TO_PIXEL;
+							break;
+						}
+						}
+						
+						coords.add(currCord.clone());
+					}
+					
+					polygons.add((double[][]) coords.toArray(new double[coords.size()][2]));
+					
+					inPath = false;
 				}
-				
-				if (line.contains("z")) inPath = false;
 			}
 			
 			if (line.contains("<path")) inPath = true;
+		}
+
+		for (double[][] poly : polygons) {
+			for (double[] coord : poly) {
+				System.out.println("x: " + coord[0] + " y: " + coord[1]);
+			}
 		}
 	}
 }
